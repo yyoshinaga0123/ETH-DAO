fe3dc8282fb5c494f96167bb2edef6fb1e8cc7b5
yyoshinaga0123 <73114407+yyoshinaga0123@users.noreply.github.com>
2023-10-15 08:23:31 +0000

    section1-lesson4からコピペ（非推奨あり）

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,40 @@
-import { ConnectWallet } from '@thirdweb-dev/react';
+// 接続中のネットワークを取得するため useNetwork を新たにインポートします。
+import { ConnectWallet, useNetwork, useAddress } from '@thirdweb-dev/react';
 import type { NextPage } from 'next';
 
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
-  return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          {/* 自分の作成する DAO 名に変更してください */}
-          Welcome to MyDAO !!
-        </h1>
-        <p></p>
-        <div className={styles.connect}>
-          <ConnectWallet />
-        </div>
-      </main>
-    </div>
-  );
+  const address = useAddress();
+  const [network, switchNetwork] = useNetwork();
+
+  if (address && network && network?.data?.chain?.id !== 11155111) {
+    console.log('wallet address: ', address);
+    console.log('network: ', network?.data?.chain?.id);
+
+    return (
+      <div className={styles.container}>
+        <main className={styles.main}>
+          <h1 className={styles.title}>Sepolia に切り替えてください⚠️</h1>
+          <p>この dApp は Sepolia テストネットのみで動作します。</p>
+          <p>ウォレットから接続中のネットワークを切り替えてください。</p>
+        </main>
+      </div>
+    );
+  } else {
+    return (
+      <div className={styles.container}>
+        <main className={styles.main}>
+          <h1 className={styles.title}>
+            Welcome to Tokyo Sauna Collective !!
+          </h1>
+          <div className={styles.connect}>
+            <ConnectWallet />
+          </div>
+        </main>
+      </div>
+    );
+  }
 };
 
 export default Home;
