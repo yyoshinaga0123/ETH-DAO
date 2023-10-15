3474098c5c128a9422cca0ecd79b5d18975d308c
yyoshinaga0123 <73114407+yyoshinaga0123@users.noreply.github.com>
2023-10-15 11:44:59 +0000

    Section2 Lesson2の途中(APIキーのエラーは解決したが実行後に終了しない)

---
+++ src/scripts/3-config-nft.ts
@@ -0,0 +1,24 @@
+import { readFileSync } from 'fs';
+
+import sdk from './1-initialize-sdk.js';
+import { editionDropAddress } from './module.js';
+
+const editionDrop = sdk.getContract(editionDropAddress, 'edition-drop');
+
+(async () => {
+  try {
+    await (
+      await editionDrop
+    ).createBatch([
+      {
+        name: "Member's symbol",
+        description:
+          'Japan Crack Organization にアクセスすることができる限定アイテムです',
+        image: readFileSync('src/scripts/assets/NFT.png'),
+      },
+    ]);
+    console.log('✅ Successfully created a new NFT in the drop!');
+  } catch (error) {
+    console.error('failed to create the new NFT', error);
+  }
+})();
---
+++ src/scripts/module.ts
@@ -0,0 +1,4 @@
+export const editionDropAddress = 'dummy_editionDropAddress';
+export const ERCTokenAddress = 'dummy_ERCTokenAddress';
+export const gavananceAddress = 'dummy_gavananceAddress';
+export const ownerWalletAddress = 'dummy_ownerWalletAddress';
