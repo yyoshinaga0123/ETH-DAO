b0c927ff06b77004a36c83f1d13e536112b88ebd
yyoshinaga0123 <73114407+yyoshinaga0123@users.noreply.github.com>
2023-10-22 10:11:02 +0000

    Section3 Lesson1まで反映

---
+++ src/scripts/6-print-money.ts
@@ -0,0 +1,24 @@
+import sdk from './1-initialize-sdk';
+import { ERCTokenAddress } from './module';
+
+// これは、前のステップで取得した私たちの ERC-20 コントラクトのアドレスです。
+const token = sdk.getContract(ERCTokenAddress, 'token');
+
+(async () => {
+  try {
+    // // 設定したい最大供給量を設定
+    const amount = 1000000;
+    // デプロイされた ERC-20 コントラクトを通して、トークンをミント
+    await (await token).mint(amount);
+    const totalSupply = await (await token).totalSupply();
+
+    // 今、私たちのトークンがどれだけあるかを表示
+    console.log(
+      '✅ There now is',
+      totalSupply.displayValue,
+      '$TSC in circulation',
+    );
+  } catch (error) {
+    console.error('Failed to print money', error);
+  }
+})();
---
+++ src/scripts/7-airdrop-token.ts
@@ -0,0 +1,50 @@
+import sdk from './1-initialize-sdk';
+import { editionDropAddress, ERCTokenAddress } from './module';
+
+// ERC-1155 メンバーシップの NFT コントラクトアドレス
+const editionDrop = sdk.getContract(editionDropAddress, 'edition-drop');
+
+// ERC-20 トークンコントラクトのアドレス
+const token = sdk.getContract(ERCTokenAddress, 'token');
+
+(async () => {
+  try {
+    // メンバーシップ NFT を所有している人のアドレスをすべて取得
+    // tokenId が 0 メンバーシップ NFT
+    const walletAddresses = await (
+      await editionDrop
+    ).history.getAllClaimerAddresses(0);
+
+    if (walletAddresses.length === 0) {
+      console.log(
+        'No NFTs have been claimed yet, maybe get some friends to claim your free NFTs!',
+      );
+    }
+
+    // アドレスの配列をループ
+    const airdropTargets = walletAddresses.map((address) => {
+      // 1000 から 10000 の間でランダムな数を取得
+      const randomAmount = Math.floor(
+        Math.random() * (10000 - 1000 + 1) + 1000,
+      );
+      console.log('✅ Going to airdrop', randomAmount, 'tokens to', address);
+
+      // ターゲットを設定
+      const airdropTarget = {
+        toAddress: address,
+        amount: randomAmount,
+      };
+
+      return airdropTarget;
+    });
+
+    // 全てのエアドロップ先で transferBatch を呼び出す
+    console.log('🌈 Starting airdrop...');
+    await (await token).transferBatch(airdropTargets);
+    console.log(
+      '✅ Successfully airdropped tokens to all the holders of the NFT!',
+    );
+  } catch (err) {
+    console.error('Failed to airdrop tokens', err);
+  }
+})();
