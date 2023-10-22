cd31b7f17f600ee653a454092a48d473148be701
yyoshinaga0123 <73114407+yyoshinaga0123@users.noreply.github.com>
2023-10-22 11:18:55 +0000

    test.tsのエラー解決のためmodule.ts修正(2)

--- src/scripts/module.ts
+++ src/scripts/module.ts
@@ -1,4 +1,4 @@
 export const editionDropAddress = 'dummy_editionDropAddress';
 export const ERCTokenAddress = 'dummy_ERCTokenAddress';
-export const govananceAddress = 'dummy_govananceAddress';
+export const governanceAddress = 'dummy_governanceAddress';
 export const ownerWalletAddress = 'dummy_ownerWalletAddress';
