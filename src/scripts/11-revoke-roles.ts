d479c705359de3e18de3f3fffe737db440981817
yyoshinaga0123 <73114407+yyoshinaga0123@users.noreply.github.com>
2023-10-22 11:11:57 +0000

    Section4 Lesson1途中まで

---
+++ src/scripts/11-revoke-roles.ts
@@ -0,0 +1,25 @@
+import sdk from './1-initialize-sdk';
+import { ERCTokenAddress } from './module';
+
+const token = sdk.getContract(ERCTokenAddress, 'token');
+
+(async () => {
+  try {
+    // 現在のロールを記録します
+    const allRoles = await (await token).roles.getAll();
+
+    console.log('👀 Roles that exist right now:', allRoles);
+
+    // ERC-20 のコントラクトに関して、あなたのウォレットが持っている権限をすべて取り消します
+    await (await token).roles.setAll({ admin: [], minter: [] });
+    console.log(
+      '🎉 Roles after revoking ourselves',
+      await (await token).roles.getAll(),
+    );
+    console.log(
+      '✅ Successfully revoked our superpowers from the ERC-20 contract',
+    );
+  } catch (error) {
+    console.error('Failed to revoke ourselves from the DAO treasury', error);
+  }
+})();
